885d602b429802e0a186335ea754dfac2bdd6fb3
Mohamed Eltazy <tazy@macusers-iMac.local>
2020-01-18 15:18:46 +0200

    Setting up the environment for the application using NXand Adding Google Angular Material as a library

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterialModule } from '@manager/material';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
